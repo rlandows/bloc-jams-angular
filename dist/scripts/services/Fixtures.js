c83e52359fb9ea020ee1e45c43de0e0a5617fa2f
Rob <rlandows@uci.edu>
2016-11-25 10:03:34 -0800

    checkpoint 6

--- dist/scripts/services/Fixtures.js
+++ dist/scripts/services/Fixtures.js
@@ -1,9 +1,13 @@
-var albumPicasso = {
+(function() {
+    function Fixture() {
+        var Fixtures = {};
+        
+        var albumPicasso = {
      title: 'The Colors',
      artist: 'Pablo Picasso',
      label: 'Cubism',
      year: '1881',
-     albumArtUrl: 'assets/images/album_covers/01.png',
+     albumArtUrl: '/assets/images/album_covers/01.png',
      songs: [
          { title: 'Blue', duration: 161.71, audioUrl: 'assets/music/blue' },
          { title: 'Green', duration: 103.96, audioUrl: 'assets/music/green' },
@@ -19,7 +23,7 @@ var albumPicasso = {
      artist: 'Guglielmo Marconi',
      label: 'EM',
      year: '1909',
-     albumArtUrl: 'assets/images/album_covers/20.png',
+     albumArtUrl: '/assets/images/album_covers/20.png',
      songs: [
          { title: 'Hello, Operator?', duration: '1:01' },
          { title: 'Ring, ring, ring', duration: '5:01' },
@@ -28,3 +32,14 @@ var albumPicasso = {
          { title: 'Wrong phone number', duration: '2:15'}
      ]
  };
+        Fixtures.getAlbum = function() {
+            return albumPicasso;
+        };
+        
+        return Fixtures;
+    }
+    
+    angular 
+        .module('blocJams')
+        .factory('Fixtures', Fixtures);
+})();
