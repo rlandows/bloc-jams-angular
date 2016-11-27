bf526a871d83252b2b960b31e336a7aef393beb5
Rob <rlandows@uci.edu>
2016-11-27 15:45:08 -0800

    checkpoint 8

--- app/scripts/services/SongPlayer.js
+++ app/scripts/services/SongPlayer.js
@@ -1,8 +1,20 @@
 (function() {
-     function SongPlayer() {
+     function SongPlayer(Fixtures) {
           var SongPlayer = {};
          
-         var currentSong = null;
+         
+         var currentAlbum = Fixtures.getAlbum();
+         
+         /**
+         *@function getSongIndex
+         @desc gets the index of a song in the song array
+         @param {Object} song
+         */
+         var getSongIndex = function(song) {
+            return currentAlbum.songs.indexOf(song);
+            };
+         
+          SongPlayer.currentSong = null;
          
           /**
         * @desc Buzz object audio file
@@ -29,7 +41,7 @@
          var setSong = function(song) {
             if (currentBuzzObject) {
             currentBuzzObject.stop();
-            currentSong.playing = null;
+            SongPlayer.currentSong.playing = null;
         }
  
             currentBuzzObject = new buzz.sound(song.audioUrl, {
@@ -37,17 +49,17 @@
                 preload: true
         });
  
-            currentSong = song;
+            SongPlayer.currentSong = song;
         };
          
          SongPlayer.play = function(song) {
-             
-             if (currentSong !== song) {
+             song = song || SongPlayer.currentSong;
+             if (SongPlayer.currentSong !== song) {
               setSong(song);
                 playSong(song);
                  
             
-        } else if (currentSong === song) {
+        } else if (SongPlayer.currentSong === song) {
             if (currentBuzzObject.isPaused()) {
                 playSong(song);
                 
@@ -56,9 +68,31 @@
      };
          
          SongPlayer.pause = function(song) {
+             song = song || SongPlayer.currentSong;
              currentBuzzObject.pause();
              song.playing = false;
          };
+         
+         /**
+         * @function SongPlayer.previous
+         *@desc gets the previous index of the song array
+         *@param {object}
+         */
+         SongPlayer.previous = function() {
+            var currentSongIndex = getSongIndex(SongPlayer.currentSong);
+            currentSongIndex--;
+             
+             if (currentSongIndex < 0) {
+                currentBuzzObject.stop();
+                SongPlayer.currentSong.playing = null;
+            } else {
+                var song = currentAlbum.songs[currentSongIndex];
+                setSong(song);
+                playSong(song);
+                }
+        };
+         
+         
          
           return SongPlayer;
      }
--- dist/scripts/services/SongPlayer.js
+++ dist/scripts/services/SongPlayer.js
@@ -1,8 +1,20 @@
 (function() {
-     function SongPlayer() {
+     function SongPlayer(Fixtures) {
           var SongPlayer = {};
          
-         var currentSong = null;
+         
+         var currentAlbum = Fixtures.getAlbum();
+         
+         /**
+         *@function getSongIndex
+         @desc gets the index of a song in the song array
+         @param {Object} song
+         */
+         var getSongIndex = function(song) {
+            return currentAlbum.songs.indexOf(song);
+            };
+         
+          SongPlayer.currentSong = null;
          
           /**
         * @desc Buzz object audio file
@@ -10,7 +22,11 @@
         */
          var currentBuzzObject = null;
          
-        
+        /**
+        *@function playSong
+        * @desc plays the song
+        @param {Object} song
+        */
          
          var playSong = function(song) {
              currentBuzzObject.play();
@@ -25,7 +41,7 @@
          var setSong = function(song) {
             if (currentBuzzObject) {
             currentBuzzObject.stop();
-            currentSong.playing = null;
+            SongPlayer.currentSong.playing = null;
         }
  
             currentBuzzObject = new buzz.sound(song.audioUrl, {
@@ -33,17 +49,17 @@
                 preload: true
         });
  
-            currentSong = song;
+            SongPlayer.currentSong = song;
         };
          
          SongPlayer.play = function(song) {
-             
-             if (currentSong !== song) {
+             song = song || SongPlayer.currentSong;
+             if (SongPlayer.currentSong !== song) {
               setSong(song);
                 playSong(song);
                  
             
-        } else if (currentSong === song) {
+        } else if (SongPlayer.currentSong === song) {
             if (currentBuzzObject.isPaused()) {
                 playSong(song);
                 
@@ -52,9 +68,31 @@
      };
          
          SongPlayer.pause = function(song) {
+             song = song || SongPlayer.currentSong;
              currentBuzzObject.pause();
              song.playing = false;
          };
+         
+         /**
+         * @function SongPlayer.previous
+         *@desc gets the previous index of the song array
+         *@param {object}
+         */
+         SongPlayer.previous = function() {
+            var currentSongIndex = getSongIndex(SongPlayer.currentSong);
+            currentSongIndex--;
+             
+             if (currentSongIndex < 0) {
+                currentBuzzObject.stop();
+                SongPlayer.currentSong.playing = null;
+            } else {
+                var song = currentAlbum.songs[currentSongIndex];
+                setSong(song);
+                playSong(song);
+                }
+        };
+         
+         
          
           return SongPlayer;
      }
