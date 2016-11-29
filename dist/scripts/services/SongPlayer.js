d900bb9e3f4c208cd57131851faef260f331c781
Rob <rlandows@uci.edu>
2016-11-29 00:14:03 -0800

    checkpoint 10

--- dist/scripts/services/SongPlayer.js
+++ dist/scripts/services/SongPlayer.js
@@ -1,5 +1,5 @@
 (function() {
-     function SongPlayer(Fixtures) {
+     function SongPlayer($rootScope, Fixtures) {
           var SongPlayer = {};
          
          
@@ -15,6 +15,12 @@
             };
          
           SongPlayer.currentSong = null;
+         
+         /**
+        * @desc Current playback time (in seconds) of currently playing song
+        * @type {Number}
+        */
+        SongPlayer.currentTime = null;
          
           /**
         * @desc Buzz object audio file
@@ -58,6 +64,12 @@
                 formats: ['mp3'],
                 preload: true
         });
+             
+            currentBuzzObject.bind('timeupdate', function() {
+                $rootScope.$apply(function() {
+                    SongPlayer.currentTime = currentBuzzObject.getTime();
+         });
+     });
  
             SongPlayer.currentSong = song;
         };
@@ -122,10 +134,22 @@
                 }
         };
          
+         /**
+         * @function setCurrentTime
+         * @desc Set current time (in seconds) of currently playing song
+         * @param {Number} time
+         */
+        SongPlayer.setCurrentTime = function(time) {
+            if (currentBuzzObject) {
+                currentBuzzObject.setTime(time);
+            }
+        };
+
+         
           return SongPlayer;
      }
  
      angular
          .module('blocJams')
-         .factory('SongPlayer', SongPlayer);
+         .factory('SongPlayer', ['$rootScope', 'Fixtures', SongPlayer]);
  })();
