f764ce310a42bc21a2db2cdb170ae60cf66bc63e
Rob <rlandows@uci.edu>
2016-11-29 11:15:24 -0800

    checkpoint 10 2

--- app/scripts/services/SongPlayer.js
+++ app/scripts/services/SongPlayer.js
@@ -144,6 +144,13 @@
                 currentBuzzObject.setTime(time);
             }
         };
+         
+         SongPlayer.setVolume = function(volume) {
+            if (currentBuzzObject) {
+                currentBuzzObject.setVolume(volume);
+            }
+            SongPlayer.volume = volume;
+        };
 
          
           return SongPlayer;
